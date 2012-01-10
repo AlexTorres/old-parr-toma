f5450a47f15e1e6a75f1750a758b6fc5b2034165
Juanda <juanzuluaga@gmail.com>
2012-01-10 13:30:09 -0500

    Added Live config and Cakephp
    We added the Live configuration options as follows:
    * config/core_live.php: Debugging disabled
    * config/database_live.php: Live database connection info
    * webroot/index_live.php: Sets the correct cakephp path
    
    Remember to remove the "_live" portion when publishing into live.

---
+++ development/php/backend/config/database_live.php
@@ -0,0 +1,13 @@
+<?php
+class DATABASE_CONFIG {
+
+	var $default = array(
+		'driver' => 'mysql',
+		'persistent' => false,
+		'host' => 'localhost',
+		'login' => 'alexgame_oldparr',
+		'password' => 'oldparr',
+		'database' => 'alexgame_oldparr',
+	);
+}
+?>
